1eccd0782ba3b01702525ea2dba44d2df31ac8a9
Gaoxin Dai <daigx1990@gmmail.com>
2020-03-13 17:27:59 -0700

    integrate python executor

--- workflows/workflow1/init.go
+++ workflows/workflow1/init.go
@@ -2,30 +2,9 @@ package workflow1
 
 import (
 	workflowFramework "github.com/flintdev/workflow-engine/engine"
-	"github.com/flintdev/workflow-engine/handler"
-	"workflow-example/workflows/workflow1/steps/step1"
-	"workflow-example/workflows/workflow1/steps/step2"
-	"workflow-example/workflows/workflow1/steps/step3"
-	"workflow-example/workflows/workflow1/steps/step4"
 )
 
 func Definition() workflowFramework.Workflow {
 	w := ParseDefinition()
 	return w
 }
-
-func Steps() map[string]func(handler handler.Handler) {
-	StepFuncMap := map[string]func(handler handler.Handler){
-		"step1": step1.Execute,
-		"step2": step2.Execute,
-		"step3": step3.Execute,
-		"step4": step4.Execute,
-	}
-	return StepFuncMap
-}
-
-func Trigger(event workflowFramework.Event) bool {
-	return TriggerCondition(event)
-}
-
-type FilePath string
